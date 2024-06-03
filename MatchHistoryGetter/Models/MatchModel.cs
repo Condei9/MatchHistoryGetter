b5ee53bfa6f89f01096fc32ecac7a14078b7150f
Condei9 <coge9989@gmail.com>
2024-06-03 23:58:28 +0300

    Added the Model necessary to deserialize the api response with the match data

---
+++ MatchHistoryGetter/Models/MatchModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MatchHistoryGetter.Models
+{
+    public class MatchModel
+    {
+        public string GameId {  get; set; }
+        public string Role { get; set; }
+        public string Season { get; set; }
+        public string PlatformId { get; set; }
+        public string Champion { get; set; }
+        public string Queue { get; set; }
+        public string Lane { get; set; }
+        public long Timestamp { get; set; }
+    }
+}
