a00d17bba4bf7d3c679dfaa370d96d2f8e484802
Condei9 <coge9989@gmail.com>
2024-06-04 02:26:37 +0300

    Added a dummy controller

---
+++ MatchHistoryGetter/Controllers/MatchHistoryController.cs
@@ -0,0 +1,19 @@
+﻿using MatchHistoryGetter.Services;
+
+namespace MatchHistoryGetter.Controllers
+{
+    public class MatchHistoryController
+    {
+        private readonly IRiotApiService _riotApiService;
+
+        public MatchHistoryController(IRiotApiService riotService)
+        {
+            _riotApiService = riotService;
+        }
+
+        public void GetMatchHistory(string summonerName, string tagLine)
+        {
+            Console.WriteLine($"not yet implemented for {summonerName}#{tagLine}");
+        }
+    }
+}
